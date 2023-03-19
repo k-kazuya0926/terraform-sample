afdee4d2d2b14eb7db801be473b8694d94b13ca7
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-03-20 08:33:49 +0900

    ACM

--- aws_route53.tf
+++ aws_route53.tf
@@ -23,3 +23,11 @@ resource "aws_route53_record" "example" {
 output "domain_name" {
   value = aws_route53_record.example.name
 }
+
+resource "aws_route53_record" "example_certificate" {
+  name    = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_type
+  records = [tolist(aws_acm_certificate.example.domain_validation_options)[0].resource_record_value]
+  zone_id = aws_route53_zone.example.id
+  ttl     = 60
+}
