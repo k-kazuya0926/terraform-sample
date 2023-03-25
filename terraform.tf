809d93093e60a892435ba6886fe50bc9380b8dc9
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-03-25 18:05:30 +0900

    Terraform Cloud

--- terraform.tf
+++ terraform.tf
@@ -6,4 +6,20 @@ terraform {
       version = "4.59.0"
     }
   }
+
+  # S3を使用する場合
+  #  backend "s3" {
+  #    backet = "tfstate-pragmatic-terraform"
+  #    key    = "example/terraform.tfstate"
+  #    region = "ap-northeast-1"
+  #  }
+
+  # Terraform Cloudを使用する場合
+  backend "remote" {
+    organization = "kazuya-kobayashi"
+
+    workspaces {
+      name = "example_workspace"
+    }
+  }
 }
