c1bd7492fe6bf62c60c718957940f17cd3432f79
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-04-01 18:03:31 +0900

    IAM Access Analyzer

--- cloud-bankruptcy-iac/aws_cloudwatch.tf
+++ cloud-bankruptcy-iac/aws_cloudwatch.tf
@@ -35,3 +35,34 @@ resource "aws_cloudwatch_event_target" "guardduty" {
 data "aws_sns_topic" "mail" {
   name = "alert-mail"
 }
+
+resource "aws_cloudwatch_event_rule" "access_analyzer" {
+  name = "access-analyzer"
+
+  event_pattern = jsonencode({
+    source      = ["aws.access-analyzer"]
+    detail-type = ["Access Analyzer Finding"]
+    detail = { # リソースを非公開にした場合などでもイベントが発火するため、ACTIVEのみを対象とする
+      status = ["ACTIVE"]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "access_analyzer" {
+  target_id = "access-analyzer"
+  rule      = aws_cloudwatch_event_rule.access_analyzer.name
+  arn       = data.aws_sns_topic.mail.arn
+
+  input_transformer {
+    input_paths = {
+      "type"     = "$.detail.resourceType"
+      "resource" = "$.detail.resource"
+      "action"   = "$.detail.action"
+    }
+
+    input_template = <<EOF
+    "<type>/<resource> allows public access."
+    "Action granted: <action>"
+    EOF
+  }
+}
