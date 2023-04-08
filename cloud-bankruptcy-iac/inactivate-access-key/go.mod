f3cc26891052bbfad49a5c38cc8b6518daf509cd
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-04-08 15:02:27 +0900

    アクセスキーの強制無効化

---
+++ cloud-bankruptcy-iac/inactivate-access-key/go.mod
@@ -0,0 +1,10 @@
+module inactivate-access-key
+
+go 1.19
+
+require (
+	github.com/aws/aws-lambda-go v1.39.1
+	github.com/aws/aws-sdk-go v1.44.239
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ cloud-bankruptcy-iac/inactivate-access-key/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
+)
+
+func handler() error {
+	sess, err := session.NewSession()
+	if err != nil {
+		return fmt.Errorf("failed NewSession: %s", err)
+	}
+	svc := iam.New(sess)
+
+	accessKeys, err := list(svc)
+	if err != nil {
+		return fmt.Errorf("failed list: %s", err)
+	}
+
+	for _, accessKey := range accessKeys {
+		if err := inactivate(svc, accessKey); err != nil {
+			return fmt.Errorf("failed inactivate: %s", err)
+		}
+	}
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
