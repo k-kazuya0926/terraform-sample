32da7d1ffd683b43ea2c442218f228509d971554
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-04-13 06:09:33 +0900

    IAMグループの権限最小化

--- cloud-bankruptcy-iac/aws_iam.tf
+++ cloud-bankruptcy-iac/aws_iam.tf
@@ -256,3 +256,65 @@ resource "aws_iam_role_policy_attachment" "example" {
   policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
   role       = aws_iam_role.example.name
 }
+
+data "aws_iam_policy_document" "assume_role_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = ["*"] # 本来は明示すべき
+  }
+}
+
+resource "aws_iam_policy" "assume_role_access" {
+  name   = "assume-role-access"
+  policy = data.aws_iam_policy_document.assume_role_access.json
+}
+
+data "aws_iam_policy_document" "rotate_access_key_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreateAccessKey",
+      "iam:UpdateAccessKey",
+      "iam:DeleteAccessKey",
+      "iam:ListAccessKeys",
+      "iam:GetAccessKeyLastUsed",
+      "iam:GetUser",
+    ]
+    resources = ["arn:aws:iam::${local.account_id}:user/&{aws:username}"] # &{aws:username}：自身のIAMユーザー名
+  }
+}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_iam_policy" "rotate_access_key_access" {
+  name   = "rotate-access-key-access"
+  policy = data.aws_iam_policy_document.rotate_access_key_access.json
+}
+
+resource "aws_iam_group" "assumable" {
+  name = "assumable"
+}
+
+resource "aws_iam_group_policy_attachment" "assume_role_access" {
+  group      = aws_iam_group.assumable.name
+  policy_arn = aws_iam_policy.assume_role_access.arn
+}
+
+resource "aws_iam_group_policy_attachment" "rotate_access_key_access" {
+  group      = aws_iam_group.assumable.name
+  policy_arn = aws_iam_policy.rotate_access_key_access.arn
+}
+
+resource "aws_iam_user" "base" {
+  name          = "base"
+  force_destroy = true
+}
+
+resource "aws_iam_group_membership" "assumable" {
+  name  = aws_iam_group.assumable.name
+  group = aws_iam_group.assumable.name
+  users = [aws_iam_user.base.name]
+}
