fd45b6088ca3f1a5b59098179cf0885cb2c1cffc
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-12-01 05:23:27 +0900

    詳解Terraform　4章

---
+++ terraform-up-and-running/04-terraform-module/module-example/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-state-kazuya-kobayashi"
+    key            = "stage/services/webserver-cluster/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "terraform-up-and-running-state-kazuya-kobayashi"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
+}
