cf06d44af93d824111a018d1a606c55159f4a3cc
Kazuya Kobayashi <kobakazu3686@gmail.com>
2023-12-03 06:50:40 +0900

    詳解Terraform　5章

--- terraform-up-and-running/global/iam/outputs.tf
+++ terraform-up-and-running/global/iam/outputs.tf
@@ -7,3 +7,10 @@ output "all_arns" {
   value       = aws_iam_user.example[*].arn
   description = "The ARNs for all users"
 }
+
+output "neo_cloudwatch_policy_arn" {
+  value = one(concat(
+    aws_iam_user_policy_attachment.neo_cloudwatch_full_access[*].policy_arn,
+    aws_iam_user_policy_attachment.neo_cloudwatch_read_only[*].policy_arn,
+  ))
+}
