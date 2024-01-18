7caad8d59b6083e08c32c78d0584f77afa3fbf86
Kazuya Kobayashi <kobakazu3686@gmail.com>
2024-01-19 06:12:42 +0900

    monthly_cost変更

--- cloud-bankruptcy-iac/aws_budgets.tf
+++ cloud-bankruptcy-iac/aws_budgets.tf
@@ -1,14 +1,14 @@
 resource "aws_budgets_budget" "monthly_cost" {
   name              = "MonthlyCost"
   budget_type       = "COST"
-  limit_amount      = "10.0"
+  limit_amount      = "15.0"
   limit_unit        = "USD" # コスト予算の場合、単位はドルのみ
   time_unit         = "MONTHLY"
   time_period_start = "2023-01-01_00:00"
 
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = 80
+    threshold                  = 100
     threshold_type             = "PERCENTAGE"
     notification_type          = "ACTUAL" # 予測値の場合は"FORECASTED"
     subscriber_email_addresses = ["test@example.com"]
