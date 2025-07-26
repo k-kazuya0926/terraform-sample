342d40448413e4c3cc63100719f73c996c12a7e7
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-07-26 18:07:05 +0900

    コスト削減のため、AWS Security Hubのベストプラクティススタンダードの設定をコメントアウト

--- cloud-bankruptcy-iac/aws_securityhub.tf
+++ cloud-bankruptcy-iac/aws_securityhub.tf
@@ -3,10 +3,11 @@
 
 resource "aws_securityhub_account" "default" {}
 
-resource "aws_securityhub_standards_subscription" "aws_best_practices" {
-  standards_arn = "arn:aws:securityhub:ap-northeast-1::standards/aws-foundational-security-best-practices/v/1.0.0"
-  depends_on    = [aws_securityhub_account.default]
-}
+# コスト削減のためコメントアウト
+# resource "aws_securityhub_standards_subscription" "aws_best_practices" {
+#   standards_arn = "arn:aws:securityhub:ap-northeast-1::standards/aws-foundational-security-best-practices/v/1.0.0"
+#   depends_on    = [aws_securityhub_account.default]
+# }
 
 # コスト削減のためコメントアウト
 #resource "aws_securityhub_standards_subscription" "cis" {
